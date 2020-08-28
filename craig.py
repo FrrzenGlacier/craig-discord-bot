e9d56bc1e863dc99718963333d3beb51f6006f2c
Glacier <frrzenglacier@gmail.com>
2020-08-28 00:26:32 -0600

    initial setup

---
+++ craig.py
@@ -0,0 +1,112 @@
+from discord.ext import commands
+import ease_of_use as eou
+import discord
+import json
+import sys
+
+
+
+# create the bot
+bot = commands.Bot(command_prefix="c.")
+
+
+
+# define getData() and setData()
+def getData():
+	with open("data.json", "r") as dataFile:
+		return json.loads(dataFile.read())
+
+def setData(_in):
+	with open("data.json", "w") as dataFile:
+		dataFile.write(json.dumps(_in, indent=4))
+
+
+
+# clear the console and load all the cogs
+eou.clear()
+cogs = getData()["cogs"]
+for cog in cogs:
+	bot.load_extension(cog)
+	eou.log(text=("%s loaded" % cog.title()))
+
+
+async def is_owner(ctx):
+	return ctx.author.id in [184474965859368960, 696087051619008612]
+
+
+@bot.event
+async def on_connect():
+	eou.log(text="Connected")
+
+
+@bot.event
+async def on_disconnect():
+	eou.log(text="Disconnected")
+
+
+@bot.event
+async def on_ready():
+	eou.log(text="Ready")
+	game = discord.Activity(type=discord.ActivityType.listening, name="2MFT")
+	await bot.change_presence(status=discord.Status.online, activity=game)
+
+
+@bot.command(name="reload", brief="Reload one or all of the bots cogs")
+@commands.check(is_owner)
+async def _reload(ctx, cog="all"):
+	try:
+		await ctx.message.delete()
+	except:
+		pass
+	log = []
+	cogs = getData()["cogs"]
+	if cog == "all":
+		for extension in cogs:
+			try:
+				bot.reload_extension(extension)
+				log.append(f"**{extension.title()}** reloaded successfully.")
+			except:
+				bot.load_extension(extension)
+				log.append(f"**{extension.title()}** loaded successfully.")
+
+		embed = eou.makeEmbed(title="Reloaded Cogs" % ctx.author.name, description="\n".join(log))
+		embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+		await ctx.send(embed=embed)
+		eou.log(text="Reloded all modules", ctx=ctx)
+	else:
+		try:
+			bot.reload_extension(cog)
+			embed = eou.makeEmbed(title=f"Reloaded {cog.title()}", description="Successfully reloaded!")
+			embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+			await ctx.send(embed=embed)
+		except:
+			bot.load_extension(cog)
+			embed = eou.makeEmbed(title=f"Loaded {cog.title()}", description="Successfully loaded!")
+			embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+			await ctx.send(embed=embed)
+		eou.log(text="Reloaded %s" % cog.title(), ctx=ctx)
+
+
+@_reload.error
+async def _reload_error(ctx, error):
+	if isinstance(error, commands.CheckFailure):
+		try:
+			await ctx.message.delete()
+		except:
+			pass
+		embed = eou.makeEmbed(title="Whoops!", description="Only the bot owner can do that command.")
+		embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+		await ctx.send(embed=embed)
+		eou.log(text="Attempted to reload cog(s) - Missing permissions", ctx=ctx)
+
+
+@bot.command(brief="Check if the bot is online")
+async def ping(ctx):
+	embed = eou.makeEmbed(title="Pong!", description="Craig is online.")
+	embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+	await ctx.send(embed=embed)
+	eou.log(text="Pinged the bot", ctx=ctx)
+
+
+with open("T:/all 2/tokens/craig.txt", "r") as token:
+	bot.run(token.read())
---
+++ ease_of_use.py
@@ -0,0 +1,37 @@
+from termcolor import colored
+from datetime import *
+import discord
+import json
+import os
+
+
+def clear(): return os.system("cls")
+
+
+def makeEmbed(title="TITLE", description="DESCRIPTION", color=0xffadb6):
+	return discord.Embed(title=title, description=description, color=color)
+
+
+def getTime():
+	return datetime.now()
+
+
+def log(text="PLACEHOLDER", cog="Main", color="green", ctx=None, event=False):
+	if ctx:
+		if event:
+			print("[%s] [%s] [%s] %s [%s] %s %s" % (getTime(), colored("Craig", "blue"), colored(cog, color), ("." * (20 - len(cog))), ("Direct Message" if not ctx.guild else ctx.guild.name), ("." * (20 - len("Direct Message" if not ctx.guild else ctx.guild.name))), text))
+		else:
+			print("[%s] [%s] [%s] %s [%s] %s [%s] %s %s" % (getTime(), colored("Craig", "blue"), colored(cog, color), ("." * (20 - len(cog))), ("Direct Message" if not ctx.guild else ctx.guild.name), ("." * (20 - len("Direct Message" if not ctx.guild else ctx.guild.name))), ctx.author.name, ("." * (20 - len(ctx.author.name))), text))
+	else:
+		print("[%s] [%s] [%s] %s %s" % (getTime(), colored("Craig", "blue"), colored(cog, color), ("." * (20 - len(cog))), text))
+
+
+# Valid Termcolor Colors:
+#  - grey
+#  - red
+#  - green
+#  - yellow
+#  - blue
+#  - magenta
+#  - cyan
+#  - white
